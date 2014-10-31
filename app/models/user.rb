aebee98fc9a86c6a2c0210ef4aa9e0e20bc28097
Ruchi Malik <rmalik2788@gmail.com>
2014-10-31 09:37:56 -0400

    trying to get my default to show

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ActiveRecord::Base
 
   validates_presence_of :username, :email, :location
 
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "assets/images/fitbud_default.jpg"
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "assets/fitbuddefault.jpg"
   validates_attachment_content_type :avatar, :content_type => ["image/jpeg", "image/gif", "image/png"]
 
   def admin?
