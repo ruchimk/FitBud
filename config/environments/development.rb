9df5152fb73af12787e007caa83220aa589f68be
Ruchi Malik <rmalik2788@gmail.com>
2014-10-31 08:59:20 -0400

    paperclip: remove Paperclip.options[:command]

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
     }
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-  Paperclip.options[:command_path] = "/usr/local/bin/"
+  # Paperclip.options[:command_path] = "/usr/local/bin/"
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
