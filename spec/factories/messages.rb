dc8a0137bd9d31ecbacbf8992fa9bb703ecbbbd8
Ruchi Malik <rmalik2788@gmail.com>
2014-10-30 20:38:30 -0400

    Made some changes to css

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,15 +1,11 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
-FactoryGirl.define do
-  factory :message do
-    sender_id 1
-    recipient_id 1
-    title "MyString"
-    body "MyText"
-  end
-end
+# FactoryGirl.define do
+#   factory :message do
+#     sender_id 1
+#     recipient_id 1
+#     title "MyString"
+#     body "MyText"
+#   end
+# end
 
-Factory.define :user do |u|
-  u.first_name { Faker::Name.first_name }
-  u.last_name { Faker::Name.last_name }
-end
