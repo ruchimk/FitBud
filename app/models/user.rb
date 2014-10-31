127f351d1916b974281c21cad06ce6b5b05924f1
Ruchi Malik <rmalik2788@gmail.com>
2014-10-31 09:41:43 -0400

    default image: fix path

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ActiveRecord::Base
 
   validates_presence_of :username, :email, :location
 
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "assets/fitbuddefault.jpg"
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "fitbuddefault.jpg"
   validates_attachment_content_type :avatar, :content_type => ["image/jpeg", "image/gif", "image/png"]
 
   def admin?
