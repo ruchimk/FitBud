b985d9f254d5ac0ce8a0333fd23e6285db45753d
Ruchi Malik <rmalik2788@gmail.com>
2014-12-22 15:12:37 -0500

    testing paperclip

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,7 +4,7 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:user).permit( :username,:email, :password, :password_confirmation, :location, :age, :goals, :admin?)
+    params.require(:user).permit(:avatar, :username,:email, :password, :password_confirmation, :location, :age, :goals, :admin?)
   end
 
   def account_update_params
